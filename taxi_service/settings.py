fb6bb9bfc611469af399da77297397a86c221b81
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2025-12-16 17:46:13 +0200

    Solution 5.0

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "crispy_forms",
-    "crispy_bootstrap4",
     "taxi",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
