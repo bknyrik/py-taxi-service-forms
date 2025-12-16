d1eb7e20a88cbbce061d0056db3cb54dfd06bf7e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2025-12-16 17:43:02 +0200

    Solution 4.0

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -141,8 +141,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
-
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
